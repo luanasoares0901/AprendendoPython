435d64802bcd82be762623408d295806cab39cd7
luanasoares0901 <202016720@aluno.unb.br>
2023-08-01 10:03:08 -0300

    add:aula 1

---
+++ codes/Aula1.py
@@ -0,0 +1,24 @@
+#Aula sobre as variáveis iniciais
+#inteiro: int
+#flutuante: float
+#booleana: bool
+#string: str
+
+idade = 38
+ano = 2023
+altura= 1.60
+verdade= True
+mentira= False
+nome= "Luana"
+sobrenome= 'Ribeiro' 
+nome_completo= nome+ " " +sobrenome
+
+print(nome_completo)    #nesse momento, as aspas não fazem diferença, mas na hora de concatenar pode dar BO
+print(nome, type(nome))
+print (sobrenome, type(sobrenome))
+print(idade)
+print(type(idade))
+print("Ano:",ano,"/ Tipo:", type(ano))
+print("Altura: ",altura,"/ Tipo:",type(altura))
+print(type(verdade))
+print(type(mentira))
